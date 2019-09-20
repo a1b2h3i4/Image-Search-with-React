11aa7dc41acfb960a6ba853f46f081d2555bea38
abhishek <abhishek27111995@gmail.com>
2019-09-20 17:01:22 +0530

    Update unspalsh.js

--- src/api/unspalsh.js
+++ src/api/unspalsh.js
@@ -2,7 +2,6 @@ import Axios from 'axios';
 export default Axios.create({
     baseURL: 'https://api.unsplash.com',
         headers:{
-            Authorization:'Client-ID a2f6d01c93faeb7cfee58eaccffeba152b5eea0bdd4477fa65df8397fdc7f89c'
         }
     }
 );
